01781fa1ac6b1f3c8676bd4d73a002d206a94549
Cryse Hillmes <tyk5555@hotmail.com>
2015-01-12 01:19:59 +0800

    1. Date 格式化工具.

---
+++ utils/src/main/java/org/cryse/utils/DateFormatUtils.java
@@ -0,0 +1,19 @@
+package org.cryse.utils;
+
+import android.text.format.DateUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatUtils {
+    public static String formatDateDividByToday(Date datetime, String todayPrefix) {
+        SimpleDateFormat formatter = null;
+        if(DateUtils.isToday(datetime.getTime())) {
+            formatter = new SimpleDateFormat("HH:mm");
+            return todayPrefix + " " + formatter.format(datetime);
+        } else {
+            formatter = new SimpleDateFormat("yyyy-MM-dd");
+            return formatter.format(datetime);
+        }
+    }
+}
