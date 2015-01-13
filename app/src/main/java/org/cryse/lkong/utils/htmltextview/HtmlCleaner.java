437cec19ce79172b2b638543e9e175bd335fee96
Cryse Hillmes <tyk5555@hotmail.com>
2015-01-13 16:34:59 +0800

    1. 解决了在 li 中有 br 标签导致的 Exception.

--- app/src/main/java/org/cryse/lkong/utils/htmltextview/HtmlCleaner.java
+++ app/src/main/java/org/cryse/lkong/utils/htmltextview/HtmlCleaner.java
@@ -11,11 +11,22 @@ public class HtmlCleaner {
         Document originalDoc = Jsoup.parse(html);
         Cleaner cleaner = new Cleaner(whitelist);
         Document fixedDoc = cleaner.clean(originalDoc);
+        for (Element liChild: fixedDoc.select("li br")) {
+            liChild.remove();
+        }
         for (Element element : fixedDoc.select("*")) {
-            if (!element.hasText() && element.isBlock()) {
+            if (!element.hasText() && element.isBlock() && !element.tagName().equalsIgnoreCase("img")
+                    && !element.tagName().equalsIgnoreCase("ul")
+                    && !element.tagName().equalsIgnoreCase("li")
+            ) {
                 element.remove();
             }
+
         }
+        // 不确定是否移除所有直属于 body 的 br 换行
+        /*for (Element brChild: fixedDoc.select("body > br")) {
+            brChild.remove();
+        }*/
         return fixedDoc.html();
     }
 }
