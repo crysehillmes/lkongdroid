1128e532ebb52817b87515a7d4c15854fcd8c3d2
Cryse Hillmes <tyk5555@hotmail.com>
2015-02-22 13:50:02 +0800

    1. 完成基本布局，但是第一次设置值后的 measure 有问题.

--- app/src/main/java/org/cryse/lkong/widget/PostItemView.java
+++ app/src/main/java/org/cryse/lkong/widget/PostItemView.java
@@ -3,6 +3,7 @@ package org.cryse.lkong.widget;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -15,12 +16,14 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import org.cryse.lkong.R;
 import org.cryse.lkong.utils.UIUtils;
 import org.cryse.utils.ColorUtils;
 
-public class PostItemView extends View {
+public class PostItemView extends RelativeLayout {
     private CharSequence mMessageText = null;
     private CharSequence mAuthorName = null;
     private CharSequence mDateline = null;
@@ -31,6 +34,9 @@ public class PostItemView extends View {
     private int px_margin_16 = 0;
     private int px_margin_72 = 0;
     private int px_width_40 = 0;
+    private int px_height_48 = 0;
+    private int px_margin_8 = 0;
+    private int px_margin_6 = 0;
 
     public PostItemView(Context context) {
         super(context);
@@ -54,26 +60,36 @@ public class PostItemView extends View {
     }
 
     private void init() {
+        setWillNotDraw(false);
         px_margin_16 = UIUtils.dp2px(getContext(), 16f);
         px_margin_72 = UIUtils.dp2px(getContext(), 72f);
         px_width_40 = UIUtils.dp2px(getContext(), 40f);
+        px_height_48 = UIUtils.dp2px(getContext(), 48f);
+        px_margin_8 = UIUtils.dp2px(getContext(), 8f);
+        px_margin_6 = UIUtils.dp2px(getContext(), 6f);
         mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
         float textSize =  getResources().getDimension(R.dimen.text_size_subhead);
         mTextPaint.setTextSize(textSize);
         mTextPaint.setColor(ColorUtils.getColorFromAttr(getContext(), R.attr.theme_text_color_primary));
+        mTextPaint.linkColor = Color.RED;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // Log.d("ViewSize", String.format("getMeasuredWidth %d, getMeasuredHeight %d", getMeasuredWidth(), getMeasuredHeight()));
         //Get the width measurement
-        int widthSize = MeasureUtils.getMeasurement(widthMeasureSpec, getDesiredWidth());
+
+        int widthSize = getMeasuredWidth();
 
         //Get the height measurement
-        int heightSize = MeasureUtils.getMeasurement(heightMeasureSpec, getDesiredHeight());
+        int heightSize = getDesiredHeight() + getMeasuredHeight() + (getMeasuredHeight() != 0 ? px_margin_6 * 2 : 0);
 
         //MUST call this to store the measurements
         setMeasuredDimension(widthSize, heightSize);
-        if((mDynamicLayout != null && mDynamicLayout.getWidth() != widthSize) || mDynamicLayout == null) {
+        Log.d("ViewSize", String.format("getMeasuredWidth %d, getMeasuredHeight %d", getMeasuredWidth(), getMeasuredHeight()));
+        if((mDynamicLayout != null && mDynamicLayout.getWidth() - px_margin_16 * 2 != widthSize) || mDynamicLayout == null) {
+            Log.d("TAGTAG", String.format("onMeasure will go to generateMessageTextLayout: widthSize: %d", widthSize));
             generateMessageTextLayout();
         }
     }
@@ -86,10 +102,6 @@ public class PostItemView extends View {
         return height;
     }
 
-    private int getDesiredWidth() {
-        return 0;
-    }
-
     public void setMessageText(CharSequence messageText) {
         if(TextUtils.equals(mMessageText, messageText)) {
             return;
@@ -100,12 +112,39 @@ public class PostItemView extends View {
     }
 
     private void generateMessageTextLayout() {
-        Log.d("TAGTAG", "generateMessageTextLayout");
-        int width = getWidth();
+        int width = getMeasuredWidth();
+        Log.d("TAGTAG", String.format("generateMessageTextLayout: width: %d", width));
         if(width > 0) {
-            mDynamicLayout = new DynamicLayout(mMessageText, mTextPaint, width, Layout.Alignment.ALIGN_NORMAL, 1.3f, 0.0f, false);
+            Log.d("TAGTAG", "generateMessageTextLayout11111");
+            mDynamicLayout = makeNewLayout(width - px_margin_16 * 2);
             requestLayout();
             invalidate();
+        } else {
+            Log.d("TAGTAG", "generateMessageTextLayout22222");
+            requestLayout();
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        final int count = getChildCount();
+        int viewWidth = getWidth();
+        int viewHeight = getHeight();
+        Log.d("ViewSize", String.format("width %d, height %d", viewWidth, viewHeight));
+        int startTop = px_margin_72 + (mDynamicLayout == null ? 0 : mDynamicLayout.getHeight());
+        int startMarginRight = px_margin_16;
+        for (int i = count - 1; i >= 0; i--) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                int left = viewWidth - (startMarginRight + child.getMeasuredWidth() + px_margin_8 * (count - 1 - i));
+                int top = startTop + px_margin_6;
+                int right = left + child.getMeasuredWidth();
+                int bottom = top + child.getMeasuredHeight();
+                Log.d("Position", String.format("left %d, top %d, right %d, bottom %d", left, top, right, bottom));
+                startMarginRight = startMarginRight + child.getMeasuredWidth() + px_margin_8 * (count - 1 - i);
+                child.layout(left, top, right, bottom);
+            }
         }
     }
 
@@ -119,13 +158,36 @@ public class PostItemView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if(mAvatarDrawable != null) {
-            mAvatarDrawable.draw(canvas);
-        }
-        if(mDynamicLayout != null) {
-            canvas.translate(0, px_margin_72);
-            mDynamicLayout.draw(canvas);
             canvas.save();
+            canvas.translate(px_margin_16, px_margin_16);
+            mAvatarDrawable.draw(canvas);
             canvas.restore();
         }
+        if(mDynamicLayout != null) {
+            canvas.save();
+            canvas.translate(px_margin_16, px_margin_72);
+            mDynamicLayout.draw(canvas);
+            canvas.restore();
+        }
+    }
+
+    public void setTextColor(int textColor) {
+        mTextPaint.setColor(textColor);
+        generateMessageTextLayout();
+    }
+
+    public void setLinkColor(int linkColor) {
+        mTextPaint.linkColor = linkColor;
+        generateMessageTextLayout();
+    }
+
+    public void setTextSize(float textSize) {
+        mTextPaint.setTextSize(textSize);
+        generateMessageTextLayout();
+    }
+
+    private DynamicLayout makeNewLayout(int wantWidth) {
+        if(isInEditMode()) return null;
+        return new DynamicLayout(mMessageText, mTextPaint, wantWidth, Layout.Alignment.ALIGN_NORMAL, 1.3f, 0.0f, false);
     }
 }
