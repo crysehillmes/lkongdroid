5b1688daaa12f2aa751f54f5563e411745c92f9f
Cryse Hillmes <tyk5555@hotmail.com>
2015-03-05 16:02:46 +0800

    1. 取消绘图缓存.

--- app/src/main/java/org/cryse/lkong/widget/PostItemView.java
+++ app/src/main/java/org/cryse/lkong/widget/PostItemView.java
@@ -80,11 +80,6 @@ public class PostItemView extends ViewGroup implements ImageSpanContainer {
 
     private void init() {
         setWillNotDraw(false);
-        setDrawingCacheEnabled(true);
-        setChildrenDrawingCacheEnabled(true);
-        setAnimationCacheEnabled(true);
-        setChildrenDrawnWithCacheEnabled(true);
-        setAlwaysDrawnWithCacheEnabled(true);
         px_margin_16 = UIUtils.dp2px(getContext(), 16f);
         px_margin_72 = UIUtils.dp2px(getContext(), 72f);
         px_width_40 = UIUtils.dp2px(getContext(), 40f);
