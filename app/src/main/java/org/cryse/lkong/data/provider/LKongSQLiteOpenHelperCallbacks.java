ed3fd6de55fce6f46bd5ae3301b57aa2ac74ee4e
Cryse Hillmes <tyk5555@hotmail.com>
2015-07-27 18:08:29 +0800

    添加了数据库 onUpgrade 的代码。

--- app/src/main/java/org/cryse/lkong/data/provider/LKongSQLiteOpenHelperCallbacks.java
+++ app/src/main/java/org/cryse/lkong/data/provider/LKongSQLiteOpenHelperCallbacks.java
@@ -33,5 +33,21 @@ public class LKongSQLiteOpenHelperCallbacks {
     public void onUpgrade(final Context context, final SQLiteDatabase db, final int oldVersion, final int newVersion) {
         if (BuildConfig.DEBUG) Log.d(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion);
         // Insert your upgrading code here.
+        if(oldVersion < 4) {
+            db.execSQL("DROP TABLE IF EXISTS " + "PINNED_FORUM_MODEL");
+            db.execSQL("DROP TABLE IF EXISTS " + "CACHE_OBJECT");
+            db.execSQL("DROP TABLE IF EXISTS " + "USER_ACCOUNT_MODEL");
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_TABLE_CACHE_OBJECT);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_CACHE_OBJECT_CACHE_KEY);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_TABLE_FOLLOWED_FORUM);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_FOLLOWED_FORUM_USER_ID);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_FOLLOWED_FORUM_FORUM_ID);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_TABLE_FOLLOWED_THREAD);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_FOLLOWED_THREAD_USER_ID);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_FOLLOWED_THREAD_THREAD_ID);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_TABLE_FOLLOWED_USER);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_FOLLOWED_USER_USER_ID);
+            db.execSQL(LKongSQLiteOpenHelper.SQL_CREATE_INDEX_FOLLOWED_USER_TARGET_USER_ID);
+        }
     }
 }
