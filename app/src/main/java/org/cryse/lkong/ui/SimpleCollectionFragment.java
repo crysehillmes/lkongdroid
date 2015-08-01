f4255cda160ef6302f2aaa7fe42443b7bd733008
Cryse Hillmes <tyk5555@hotmail.com>
2015-08-01 15:44:08 +0800

    修复 SimpleCollectionFragment 中重复加载的问题。

--- app/src/main/java/org/cryse/lkong/ui/SimpleCollectionFragment.java
+++ app/src/main/java/org/cryse/lkong/ui/SimpleCollectionFragment.java
@@ -107,13 +107,16 @@ public abstract class SimpleCollectionFragment<
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        if (mItemList.size() == 0) {
+            if (savedInstanceState != null && savedInstanceState.containsKey(DataContract.BUNDLE_CONTENT_LIST_STORE)) {
 
-        if(savedInstanceState != null && savedInstanceState.containsKey(DataContract.BUNDLE_CONTENT_LIST_STORE)) {
-            ArrayList<ItemType> list = savedInstanceState.getParcelableArrayList(DataContract.BUNDLE_CONTENT_LIST_STORE);
-            mCollectionAdapter.addAll(list);
-            mLastItemSortKey = savedInstanceState.getLong(DataContract.BUNDLE_THREAD_LIST_LAST_SORTKEY);
-        } else {
-            loadInitialData();
+                ArrayList<ItemType> list = savedInstanceState.getParcelableArrayList(DataContract.BUNDLE_CONTENT_LIST_STORE);
+                mCollectionAdapter.addAll(list);
+                mLastItemSortKey = savedInstanceState.getLong(DataContract.BUNDLE_THREAD_LIST_LAST_SORTKEY);
+
+            } else {
+                loadInitialData();
+            }
         }
     }
 
