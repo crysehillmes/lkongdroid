ef3b2fc513ca3ae8ef7b97cd8325afa3d2c887d2
Cryse Hillmes <tyk5555@hotmail.com>
2016-01-20 01:14:52 +0800

    暂时移除 Google Analytics

--- app/src/main/java/org/cryse/lkong/application/LKongApplication.java
+++ app/src/main/java/org/cryse/lkong/application/LKongApplication.java
@@ -2,7 +2,7 @@ package org.cryse.lkong.application;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
+/*import android.support.multidex.MultiDex;*/
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -38,11 +38,11 @@ public class LKongApplication extends Application {
     private SendServiceComponet mSendServiceComponet;
     private UserAccountManager mUserAccountManager;
 
-    @Override
+    /*@Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
-    }
+    }*/
 
     @Override
     public void onCreate() {
