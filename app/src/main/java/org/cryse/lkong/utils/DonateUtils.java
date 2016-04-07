d5687e78948c6462111a6dbc9110c200e353a37b
Cryse Hillmes <tyk5555@hotmail.com>
2016-04-07 13:56:35 +0800

    修改捐赠逻辑。

--- app/src/main/java/org/cryse/lkong/utils/DonateUtils.java
+++ app/src/main/java/org/cryse/lkong/utils/DonateUtils.java
@@ -1,8 +1,12 @@
 package org.cryse.lkong.utils;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
@@ -15,9 +19,18 @@ public class DonateUtils {
                 .content(R.string.donation_content)
                 .positiveText(R.string.donation_positive)
                 .onPositive((dialog, which) -> {
-                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.alipay.com"));
-                    activity.startActivity(browserIntent);
+                    ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("tyk5555@hotmail.com", "tyk5555@hotmail.com");
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(activity, activity.getString(R.string.donation_clipboard_toast), Toast.LENGTH_SHORT).show();
+                    //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.alipay.com"));
+                    //activity.startActivity(browserIntent);
+                    Intent startAlipayIntent = activity.getPackageManager().getLaunchIntentForPackage("com.eg.android.AlipayGphone");
+                    if(startAlipayIntent == null) {
+                        startAlipayIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.alipay.com"));
+                    }
                     dialog.dismiss();
+                    activity.startActivity(startAlipayIntent);
                 })
                 .show();
     }
